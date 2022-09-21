dd8873dd29cba7d6ae85f7b9d56a48f43959fdf1
JB Manillier <jbmanillier@gmail.com>
2022-09-21 08:30:41 +0200

    lolilol

--- pages/index.tsx
+++ pages/index.tsx
@@ -1007,29 +1007,6 @@ const Home: React.FC = () => {
                 
 
 
-
-                <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {people.map((person) => (
-        <li key={person.email} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white dark:bg-slate-700/30 dark:highlight-white/5 shadow-md shadow-gray-200/60">
-          <div className="flex w-full items-center justify-between space-x-6 p-6">
-            <div className="flex-1 truncate">
-              <div className="flex items-center space-x-3">
-                <h3 className="truncate text-sm font-medium text-gray-900 dark:text-white/90">{person.name}</h3>
-                {/*<span className="inline-block flex-shrink-0 rounded-full bg-green-100 dark:bg-slate-600 e px-2 py-0.5 text-xxs font-medium text-green-800 dark:text-slate-400">
-                  {person.role}
-                </span>*/}
-              </div>
-              <p className="mt-1 truncate text-sm text-gray-500 dark:text-slate-400 font-light">{person.title}</p>
-            </div>
-            <img className="h-10 w-10 flex-shrink-0 rounded-lg bg-gray-300" src={person.imageUrl} alt="" />
-          </div>
-        </li>
-      ))}
-    </ul>
-
-
-
-
     <div className="relative pt-16 pb-20 lg:pt-12 lg:pb-28">
 
       <div className="relative mx-auto">
